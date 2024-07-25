d1d5685ae3d3428c7b609cf9e14db8fdc61b902a
Gabriel_Duarte <88411632+gabriel2036@users.noreply.github.com>
2024-07-25 11:25:09 -0300

    add pastas Tad e a estrutura da palavra
    A estrutura  que iremos trabalhar está no .h do tad READ

---
+++ Tad Read/read.c
@@ -0,0 +1,5 @@
+//
+// Created by gabri on 25/07/2024.
+//
+
+#include "read.h"
---
+++ Tad Read/read.h
@@ -0,0 +1,14 @@
+
+#ifndef AEDS_2_TP_READ_H
+#define AEDS_2_TP_READ_H
+
+
+typedef struct ingredient{
+    char *name;
+    // colocar a lista aqui
+}ingredient;
+
+
+
+
+#endif //AEDS_2_TP_READ_H
