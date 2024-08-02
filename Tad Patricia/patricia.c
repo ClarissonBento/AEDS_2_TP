dc0a81af7b4b7d1b68870815c08466a8af84f64c
Gabriel_Duarte <88411632+gabriel2036@users.noreply.github.com>
2024-08-02 10:30:37 -0300

    Criação da arvore patricia

--- Tad Patricia/patricia.c
+++ Tad Patricia/patricia.c
@@ -1,89 +1,257 @@
-// colocar o nome dos membros
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <ctype.h>
 
 
-#include "patricia.h"
 
 
 short IsExternal(Tree p) {
     return (p->type == EXTERNAL);
 }
 
-int SelectPath(Index index, Ingredient key, int i) {
-    int max_size = (int)strlen(key.name);
-    int id = (int) index.letter;
-    int key_char = (int) key.name[i];
-    if (index.position >= max_size || id > key_char) {
-        return 0; // \0 is placed on the left side of the tree
+int SelectPath(Index index, Ingredient key, Tree p) {
+    int keyLen = (int)strlen(key.name);
+    if (index.position < keyLen && key.name[index.position] != index.letter) {
+        return (key.name[index.position] > index.letter) ? 1 : 0;
     } else {
         return 1;
     }
 }
 
+int Path(Index Int_Index, char *Ingredient_name) {
+
+    if(Ingredient_name[Int_Index.position]  >=  Int_Index.letter){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+char Internal_char_selection(char *TreeWord, char *NewWord, int Diff_Index) {
+    return (TreeWord[Diff_Index] > NewWord[Diff_Index]) ? TreeWord[Diff_Index] : NewWord[Diff_Index];
+}
+
 Tree CreateExternalNode(Ingredient key) {
     Tree p = (Tree) malloc(sizeof(PatNode));
-    p->node.item.name = (char *)malloc((strlen(key.name) + 1) * sizeof(char));
-    p->node.item = key;
+    p->node.item.name = strdup(key.name);
     p->type = EXTERNAL;
     return p;
 }
 
-Tree CreateInternalNode(int i, Tree *left, Tree *right, char diff) {
-    Tree temp;
-    temp = (Tree) malloc(sizeof(PatNode));
+Tree CreateInternalNode(int i, Tree left, Tree right, char diff) {
+    Tree temp = (Tree) malloc(sizeof(PatNode));
     temp->type = INTERNAL;
-    temp->node.InternalNode.left = *left;
-    temp->node.InternalNode.right = *right;
+    temp->node.InternalNode.left = left;
+    temp->node.InternalNode.right = right;
     temp->node.InternalNode.index.position = i;
     temp->node.InternalNode.index.letter = diff;
     return temp;
 }
 
-Tree InsertBetween(Ingredient key, Tree *p, int i, char diff) {
-    if (IsExternal(*p) || i < (*p)->node.InternalNode.index.position) {
-        // create external node
+Tree InsertBetween(Ingredient key, Tree *p, int i, char diff, char *compared_char) {
+    if (IsExternal(*p)) {
+
+        diff = Internal_char_selection((*p)->node.item.name, key.name, i);
         Tree temp = CreateExternalNode(key);
-        if (SelectPath((*p)->node.InternalNode.index, key, i) == 1) {
-            return(CreateInternalNode(i, p, &temp, diff));
+        Tree New;
+        if (strcmp(key.name, (*p)->node.item.name) == 0  || (strcmp(key.name, (*p)->node.item.name) > 0 )){
+            New = CreateInternalNode(i, *p, temp, diff);
+            return New;
         } else {
-            return(CreateInternalNode(i, &temp, p, diff));
+
+            New = CreateInternalNode(i, temp, *p, diff);
+            return New;
+        }
+    } else if (i < (*p)->node.InternalNode.index.position) {// Se o indice do no for menor que o indice que difere os caracteres da palavra
+        con++;
+        Tree temp = CreateExternalNode(key);
+        if (strcmp(key.name, compared_char) == 0 || (strcmp(key.name, compared_char) > 0)){ // Se a palavra que insere for maior ou igual que a palavra do no
+            return CreateInternalNode(i, *p, temp, diff); // o indice de comparação do no interno recebe o caracter da palavra que entra,
+        } else {
+            return CreateInternalNode(i, temp, *p, compared_char[i]); // o indice de comparação do no interno recebe o caracter da palavra que já estava no no que comparamos na inseração
         }
     } else {
-        if (SelectPath((*p)->node.InternalNode.index, key, i) == 1) {
-            (*p)->node.InternalNode.right = InsertBetween(key, &(*p)->node.InternalNode.right, i, diff);
+        if (Path((*p)->node.InternalNode.index, key.name)) {
+            (*p)->node.InternalNode.right = InsertBetween(key, &(*p)->node.InternalNode.right, i, diff, compared_char);
         } else {
-            (*p)->node.InternalNode.left = InsertBetween(key, &(*p)->node.InternalNode.left, i, diff);
+            (*p)->node.InternalNode.left = InsertBetween(key, &(*p)->node.InternalNode.left, i, diff, compared_char);
         }
-        return (*p);
+        return *p;
     }
 }
 
 Tree Insert(Ingredient key, Tree *p) {
     if (*p == NULL) {
-        return(CreateExternalNode(key));
+        return CreateExternalNode(key);
     }
     Tree temp = *p;
-    int i = 0;
+    int i;
     while (!IsExternal(temp)) {
-        if (SelectPath(temp->node.InternalNode.index, key, i) == 1) {
+        if (Path(temp->node.InternalNode.index, key.name)) {
             temp = temp->node.InternalNode.right;
-            i++;
         } else {
             temp = temp->node.InternalNode.left;
-            i++;
         }
     }
     int max_size = (int)strlen(key.name);
     i = 0;
-    while ((i < max_size) && ((int)key.name[i] == (int)temp->node.item.name[i])) {
+    while (i < max_size && key.name[i] == temp->node.item.name[i]) {
         i++;
     }
-    if (i >= max_size) {
-        printf("Handle here\n");
+
+    if(strcmp(key.name, temp->node.item.name) == 0){
+        printf("Palavra já existe\n");
         return *p;
-    } else {
-        return InsertBetween(key, p, i, key.name[i]);
     }
+
+    char diff = Internal_char_selection(temp->node.item.name, key.name, i);
+    return InsertBetween(key, p, i, diff, temp->node.item.name);
+
+
+}
+
+Tree search(Tree *p, Ingredient key) {
+    Tree temp = *p;
+    while (!IsExternal(temp)) {
+        if (Path(temp->node.InternalNode.index, key.name)) {
+            temp = temp->node.InternalNode.right;
+        } else {
+            temp = temp->node.InternalNode.left;
+        }
+    }
+    if (strcmp(temp->node.item.name, key.name) == 0) {
+        printf("Palavra encontrada\n");
+        return temp;
+    } else {
+        printf("Palavra não encontrada, %s\n", key.name);
+        return NULL;
+    }
+}
+
+void print_tree(Tree p) {
+
+    if (p == NULL) {
+        return;
+    }
+    if (!IsExternal(p)) {
+        print_tree(p->node.InternalNode.left);
+
+    }
+    if (IsExternal(p)) {
+        printf("%s\n", p->node.item.name);
+        con++;
+    }
+    if (!IsExternal(p)) {
+        print_tree(p->node.InternalNode.right);
+    }
+}
+
+void free_tree(Tree p) {
+    if (p == NULL) {
+        return;
+    }
+    if (!IsExternal(p)) {
+        free_tree(p->node.InternalNode.left);
+        free_tree(p->node.InternalNode.right);
+    } else {
+        free(p->node.item.name);
+    }
+    free(p);
 }
 
 
 
+void InsertFromFile(const char *filename, Tree *patricia) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Unable to open file");
+        return;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), file)) {
+        line[strcspn(line, "\n")] = '\0';
+
+        Ingredient t;
+        t.name = strdup(line);
+
+        if(strcmp(t.name, "yogurt") == 0){
+            printf("z\n");
+            /*Ingredient tst;
+            tst.name = "banana";
+            search(patricia, tst);*/
+        }
+
+
+        *patricia = Insert(t, patricia);
+        /*if(con > 0){
+            print_tree(*patricia);
+            printf("............................................\n");
+            con = 0;
+        }*/
+    }
+
+    fclose(file);
+}
+
+void SearchFromFile(const char *filename, Tree *patricia) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Unable to open file");
+        return;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), file)) {
+        line[strcspn(line, "\n")] = '\0';
+        Tree temp;
+        Ingredient t;
+        t.name = strdup(line);
+
+        /*if(strcmp(t.name, "cherry") == 0){
+            printf("z\n");
+            Ingredient tst;
+            tst.name = "banana";
+            search(patricia, tst);
+        }*/
+
+
+        temp = search(patricia, t);
+        /*if(con > 0){
+            print_tree(*patricia);
+            printf("............................................\n");
+            con = 0;
+        }*/
+    }
+
+    fclose(file);
+}
+
+
+
+
+int main(void) {
+    Tree patricia = NULL;
+
+    testPatricia();
+    printf("Com = %d\n", con);
+
+
+    InsertFromFile("words2.txt", &patricia);
+
+    Ingredient tst;
+    tst.name = "egg";
+    patricia = Insert(tst, &patricia);
+
+    print_tree(patricia);
+    printf("Com = %d\n", con2);
+
+    SearchFromFile("words2.txt", &patricia);
+    printf("Total de palavras = %d\n", con);
+    return 0;
+
+
+
+}
--- Tad Patricia/patricia.h
+++ Tad Patricia/patricia.h
@@ -4,6 +4,34 @@
 #define AEDS_2_TP_PATRICIA_H
 
 #include "read.h"
+typedef enum {
+    INTERNAL, EXTERNAL
+} NodeType;
+
+typedef struct Index {
+    char letter;
+    int position;
+} Index;
+
+typedef struct Ingredient {
+    char *name;
+} Ingredient;
+
+typedef char *IngredientType;
+
+typedef struct pat *Tree;
+typedef struct pat {
+    NodeType type;
+    union {
+        struct {
+            Index index;
+            Tree left, right;
+        } InternalNode;
+        Ingredient item;
+    } node;
+} PatNode;
+
+
 
 Tree Insert(Ingredient key, Tree *p);
 Tree search(Tree *p, Ingredient key);
